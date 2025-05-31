3d17ac38e085082b7b5c048ff74a5c9cbca16957
fverse <mail.fawazabdulla@gmail.com>
2025-05-31 12:30:47 +0530

    add solution for problem 6

--- problems/prob.06.ml
+++ problems/prob.06.ml
@@ -1,1 +1,17 @@
 (* Palindrome *)
+
+let rev (xs: 'a list): 'a list =
+  let rec rev' (xs: 'a list) (acc: 'a list): 'a list =
+    match xs with
+    | [] -> acc
+    | x :: rest -> rev' rest (x :: acc)
+  in rev' xs []
+
+let is_palindrome (ls: 'a list): bool =
+   match ls with
+    | [] -> true
+    | [_] -> true
+    | _ -> ls = rev ls
+;;
+
+(* print_endline (string_of_bool (is_palindrome [1; 2])) *)
